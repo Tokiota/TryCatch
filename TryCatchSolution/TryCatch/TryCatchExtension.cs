87e6a8aeb5a8aef2aaf85651bc915530f7871934
Tokiota <daniel.mazzini@tokiota.com>
2013-02-15 00:20:45 +0100

    el primero

---
+++ TryCatchSolution/TryCatch/TryCatchExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryCatch
+{
+    public static class TryCatchExtension
+    {
+        public static void TryCatch<TException>(this Action tryAction, Action<TException> onException) where TException : Exception
+        {
+
+        }
+    }
+}
