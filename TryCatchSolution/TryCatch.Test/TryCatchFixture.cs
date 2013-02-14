f52ef3f2da5cd0e4d1ebc8a9dd9eb76f633095a8
Tokiota <daniel.mazzini@tokiota.com>
2013-02-15 00:25:19 +0100

    pruebas

---
+++ TryCatchSolution/TryCatch.Test/TryCatchFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryCatch.Test
+{
+    [TestClass]
+    public class TryCatchFixture
+    {
+        [TestMethod]
+        public void CuandoHayUnaException()
+        {
+            Action generarExcepcion = () =>
+            {
+                int divisor = 0;
+                var result = 10 / divisor;
+            };
+            Action<DivideByZeroException> whenError = exception => Assert.IsInstanceOfType(exception, typeof(DivideByZeroException));
+
+            generarExcepcion.TryCatch(whenError);
+        }
+    }
+
+}
