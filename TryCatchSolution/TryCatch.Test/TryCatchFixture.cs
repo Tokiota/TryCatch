42767cb70a716ea4ac6815a3c468925548c1cea2
Tokiota <daniel.mazzini@tokiota.com>
2013-02-15 00:54:19 +0100

    Se agregan pruebas

--- TryCatchSolution/TryCatch.Test/TryCatchFixture.cs
+++ TryCatchSolution/TryCatch.Test/TryCatchFixture.cs
@@ -10,14 +10,54 @@ namespace TryCatch.Test
         public void CuandoHayUnaException()
         {
             Action generarExcepcion = () =>
-            {
-                int divisor = 0;
-                var result = 10 / divisor;
-            };
+                {
+                    int divisor = 0;
+                    var result = 10 / divisor;
+                };
             Action<DivideByZeroException> whenError = exception => Assert.IsInstanceOfType(exception, typeof(DivideByZeroException));
 
             generarExcepcion.TryCatch(whenError);
         }
-    }
 
+        [TestMethod]
+        public void CuandoNoHayUnaException()
+        {
+            var ejecutoCatch = false;
+
+            Action noGeneraExcepcion = () =>
+                {
+                    int divisor = 2;
+                    var result = 10 / divisor;
+                };
+            Action<DivideByZeroException> whenError = exception =>
+                {
+                    ejecutoCatch = true;
+                    Assert.IsInstanceOfType(exception, typeof(DivideByZeroException));
+                };
+
+            noGeneraExcepcion.TryCatch(whenError);
+
+            Assert.IsFalse(ejecutoCatch);
+        }
+
+        [TestMethod]
+        public void CuandoHayUnaExceptionGenerica()
+        {
+            bool ejecutoCatch = false;
+            Action generaExcepcionGenerica = () =>
+                {
+                    throw new Exception("Mensaje");
+                };
+            Action<DivideByZeroException> whenError = exception =>
+                {
+                    ejecutoCatch = true;
+                };
+            Action<Exception> whenGenerico = exception => Assert.IsInstanceOfType(exception, typeof(Exception));
+
+            generaExcepcionGenerica.TryCatch(whenError, whenGenerico);
+
+            Assert.IsFalse(ejecutoCatch);
+        }
+
+    }
 }
--- TryCatchSolution/TryCatch/TryCatchExtension.cs
+++ TryCatchSolution/TryCatch/TryCatchExtension.cs
@@ -6,7 +6,28 @@ namespace TryCatch
     {
         public static void TryCatch<TException>(this Action tryAction, Action<TException> onException) where TException : Exception
         {
+            try
+            {
+                tryAction.Invoke();
+            }
+            catch(TException exception)
+            {
+                onException(exception);
+            }
+        }
 
+        public static void TryCatch<TException1, TException2>(this Action tryAction, Action<TException1> onException1, Action<TException2> onException2)
+            where TException1 : Exception
+            where TException2 : Exception
+        {
+            try
+            {
+                tryAction.TryCatch(onException1);
+            }
+            catch (TException2 exception)
+            {
+                onException2.Invoke(exception);
+            }
         }
     }
 }
